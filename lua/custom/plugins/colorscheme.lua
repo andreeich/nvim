6e16741741fd34d6df608560912a435cb528aa8a
andreeich <andreei.chervoniak@icloud.com>
2026-01-02 15:18:21 +0100

    configure leap labels

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -40,7 +40,7 @@ return {
     'nyoom-engineering/oxocarbon.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'oxocarbon'
+      -- vim.cmd.colorscheme 'oxocarbon'
     end,
   },
 }
--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,6 +1,7 @@
 return {
   {
     'ggandor/leap.nvim',
+    labels = 'sfnjklhodweimbuyvrgtaqpcxz',
     config = function()
       vim.keymap.set({ 'n', 'o' }, 's', '<Plug>(leap)', { desc = 'Leap in window' })
       vim.keymap.set('n', '<S-s>', '<Plug>(leap-from-window)', { desc = 'Leap from window' })
