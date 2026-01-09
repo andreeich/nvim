ee3befe70f6e601cdd29c655bdf0a597d8784dec
andreeich <andreei.chervoniak@icloud.com>
2026-01-09 10:17:49 +0100

    Change gitsigns options

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -44,24 +44,14 @@ return {
         end, { desc = 'Jump to previous git [c]hange' })
 
         -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'git [s]tage hunk' })
-        map('v', '<leader>hr', function()
+        map('v', 'dp', function()
           gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'git [r]eset hunk' })
+        end, { desc = 'git reset hunk' })
         -- normal mode
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-        map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-        map('n', '<leader>ho', gitsigns.preview_hunk_inline, { desc = 'git [o]pen hunk' })
-        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-        map('n', '<leader>hD', function()
+        map('n', 'dp', gitsigns.reset_hunk, { desc = 'git reset hunk' })
+        map('n', 'dP', gitsigns.reset_buffer, { desc = 'git reset buffer' })
+        map('n', 'do', gitsigns.preview_hunk, { desc = 'git preview hunk' })
+        map('n', 'dO', function()
           gitsigns.diffthis '@'
         end, { desc = 'git [D]iff against last commit' })
         -- Toggles
