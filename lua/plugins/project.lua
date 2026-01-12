e692c769b8609b6b119044740c39cba717ac50c9
andreeich <andreei.chervoniak@icloud.com>
2026-01-13 00:19:03 +0100

    Update project plugin config

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -3,7 +3,14 @@ return {
   config = function()
     require('project_nvim').setup {
       detection_methods = { 'pattern' },
-      patterns = { '.git', 'package.json', 'tsconfig.json' },
+      patterns = {
+        '.git',
+        'pnpm-workspace.yaml',
+        'yarn.lock',
+        'package-lock.json',
+        'turbo.json',
+        'nx.json',
+      },
     }
   end,
 }
