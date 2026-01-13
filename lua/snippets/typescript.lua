16b5c03dba16fd8746444b5b03604c78731cc557
andreeich <andreei.chervoniak@icloud.com>
2026-01-13 22:25:19 +0100

    Add typescript snippet

---
+++ lua/snippets/typescript.lua
@@ -0,0 +1,22 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+
+  s('clg', {
+    t 'console.log(',
+    i(1),
+    t { ');' },
+  }),
+  s('clo', {
+    t 'console.log(',
+    t { '"' },
+    i(1),
+    t { '"' },
+    t { ', ' },
+    i(1),
+    t { ');' },
+  }),
+}
