14353f677bf5ecab53c7dc099f1e3606f55529f5
andreeich <andreei.chervoniak@icloud.com>
2026-01-18 16:31:18 +0100

    Add nvim-treesitter-textobjects as a dependency

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,6 +4,10 @@ return {
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  dependencies = {
+    -- Add textobjects for mini.ai and other plugins
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
   opts = {
     ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
     -- Autoinstall languages that are not installed
